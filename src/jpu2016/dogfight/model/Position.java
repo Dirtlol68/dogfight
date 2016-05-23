f921f124d3ebe76cf495895cbef0b4fe44866483
Clemsouille <clement.rosenberger@viacesi.fr>
2016-05-23 17:18:19 +0200

    Phase 3 en cours

--- src/jpu2016/dogfight/model/Position.java
+++ src/jpu2016/dogfight/model/Position.java
@@ -4,4 +4,35 @@ package jpu2016.dogfight.model;
  * Created by Clemsouille on 23/05/2016.
  */
 public class Position {
+
+    private double x ;
+    private double y ;
+    private double maxX ;
+    private double maxY ;
+
+
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    protected void setMaxX(double maxX) {
+        this.maxX = maxX;
+    }
+
+    protected void setMaxY(double maxY) {
+        this.maxY = maxY;
+    }
 }
