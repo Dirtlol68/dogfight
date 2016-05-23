8b5c21a3817a8fb2bbf8b98bc63c7b6e20dec4ec
Shiks67 <ahmed.sdlv@gmail.com>
2016-05-23 17:09:30 +0200

    phase 2.0 en cours

---
+++ src/jpu2016/dogfight/view/IViewSystem.java
@@ -0,0 +1,12 @@
+package jpu2016.dogfight.view;
+
+/**
+ * Created by Ahmed on 23/05/2016.
+ */
+public interface IViewSystem {
+
+    public String displayMessage(String message);
+
+    public void closeAll();
+
+}
--- src/jpu2016/gameframe/GameFrame.java
+++ src/jpu2016/gameframe/GameFrame.java
@@ -1,7 +1,26 @@
 package jpu2016.gameframe;
 
+import java.awt.event.KeyEvent;
+import java.util.Observable;
+
 /**
  * Created by Ahmed on 23/05/2016.
  */
 public class GameFrame {
+
+    public GameFrame(String title, IEventPerformer performer, IGraphicsBuilder graphicsBuilder, Observable observable){
+
+    }
+
+    public void keyPressed(KeyEvent keyEvent){
+
+    }
+
+    public void keyReleased(KeyEvent keyEvent){
+
+    }
+
+    public void keyTyped(KeyEvent keyEvent){
+
+    }
 }
