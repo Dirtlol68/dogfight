4baa743b341437f127cd69b39e20f02388746328
Votre nom ou pseudo <arno.birchler@viacesi.fr>
2016-05-24 20:35:54 +0200

    Phase 4 en cours

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,10 @@ public class Main {
         int HEIGHT = 600, WIDTH = 600;
         final DogfightModel dogfightModel = new DogfightModel();
         Sky sky = new Sky(new Dimension(HEIGHT,WIDTH));
-        dogfightModel.addMobile(new Cloud(Direction.UP, new Position(200,200,HEIGHT,WIDTH)));
-        dogfightModel.addMobile(new Plane(1, Direction.RIGHT, new Position(100,100,HEIGHT,WIDTH), "Zero_RIGHT"));
-        dogfightModel.addMobile(new Plane(2, Direction.LEFT, new Position(500,100,HEIGHT,WIDTH), "F4U_RIGHT"));
-        dogfightModel.addMobile(new Cloud(Direction.UP, new Position(400,400, HEIGHT,WIDTH)));
+        dogfightModel.addMobile(new Cloud(Direction.UP, new Position(HEIGHT/4,WIDTH/4,HEIGHT,WIDTH)));
+        dogfightModel.addMobile(new Plane(1, Direction.RIGHT, new Position(51,WIDTH*0.1,HEIGHT,WIDTH), "Zero_RIGHT"));
+        dogfightModel.addMobile(new Plane(2, Direction.LEFT, new Position(HEIGHT - 101,WIDTH*0.9,HEIGHT,WIDTH), "F4U_LEFT"));
+        dogfightModel.addMobile(new Cloud(Direction.UP, new Position(HEIGHT*3/4,WIDTH*3/4, HEIGHT,WIDTH)));
 
         final DogfightController dogfightController = new DogfightController(dogfightModel);
         final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel,
