b59ba0fa955fca423747211d772cdb3c0722537a
Clemsouille <clement.rosenberger@viacesi.fr>
2016-05-24 11:54:17 +0200

    Phase 3 en cours

--- src/jpu2016/dogfight/model/Mobile.java
+++ src/jpu2016/dogfight/model/Mobile.java
@@ -1,5 +1,7 @@
 package jpu2016.dogfight.model;
 
+import java.awt.*;
+
 /**
  * Created by Clemsouille on 23/05/2016.
  */
@@ -9,5 +11,75 @@ public class Mobile implements IMobile {
 
     public Mobile(Direction direction, Position position, Dimension dimension, int speed, String image) {
         this.speed = speed;
+    }
+
+    @Override
+    public Direction getDirection() {
+        return null;
+    }
+
+    @Override
+    public void setDirection(Direction direction) {
+
+    }
+
+    @Override
+    public Point getPosition() {
+        return null;
+    }
+
+    @Override
+    public Dimension getDimension() {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getSpeed() {
+        return 0;
+    }
+
+    @Override
+    public Image getImage() {
+        return null;
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void placeInArea(IArea area) {
+
+    }
+
+    @Override
+    public boolean isPlayer(int player) {
+        return false;
+    }
+
+    @Override
+    public void setDogfightModel(DogfightModel dogfightModel) {
+
+    }
+
+    @Override
+    public boolean hit() {
+        return false;
+    }
+
+    @Override
+    public boolean isWeapon() {
+        return false;
     }
 }
