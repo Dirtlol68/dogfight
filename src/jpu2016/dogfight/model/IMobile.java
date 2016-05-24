eb444c2130fe33793f11d8c53f18f5c58832990c
Clemsouille <clement.rosenberger@viacesi.fr>
2016-05-24 11:49:39 +0200

    Phase 3 en cours

--- src/jpu2016/dogfight/model/IMobile.java
+++ src/jpu2016/dogfight/model/IMobile.java
@@ -1,8 +1,77 @@
 package jpu2016.dogfight.model;
 
+import java.awt.*;
+
 /**
  * Created by Clemsouille on 23/05/2016.
  */
-public class IMobile {
+public interface IMobile {
+
+    public Direction getDirection(){
+
+        return null;
+    }
+
+
+    public void setDirection(Direction direction){
+
+    }
+
+    public Point getPosition(){
+
+        return null;
+    }
+
+    public Dimension getDimension(){
+
+        return null;
+    }
+
+    public int getWidth(){
+
+        return 1;
+    }
+
+    public int getHeight(){
+
+        return 1;
+    }
+
+    public int getSpeed(){
+
+        return 1;
+    }
+
+    public Image getImage(){
+
+        return null;
+    }
+
+    public void move(){
+
+    }
+
+    public void placeInArea(IArea area){
+
+    }
+
+    public boolean isPlayer(int player){
+
+        return true;
+    }
+
+    public void setDogfightModel(DogfightModel dogfightModel){
+
+    }
+
+    public boolean hit(){
+
+        return true;
+    }
+
+    public boolean isWeapon(){
+
+        return true;
+    }
 
 }
--- src/jpu2016/dogfight/model/Mobile.java
+++ src/jpu2016/dogfight/model/Mobile.java
@@ -3,5 +3,11 @@ package jpu2016.dogfight.model;
 /**
  * Created by Clemsouille on 23/05/2016.
  */
-public class Mobile {
+public class Mobile implements IMobile {
+
+    private int speed ;
+
+    public Mobile(Direction direction, Position position, Dimension dimension, int speed, String image) {
+        this.speed = speed;
+    }
 }
