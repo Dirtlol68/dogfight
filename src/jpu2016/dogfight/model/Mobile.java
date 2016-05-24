d650fb36fa798a990f4c33b518e6aa21b4def10c
Votre nom ou pseudo <arno.birchler@viacesi.fr>
2016-05-24 20:00:06 +0200

    Phase 4 en cours

--- src/jpu2016/dogfight/model/Mobile.java
+++ src/jpu2016/dogfight/model/Mobile.java
@@ -46,12 +46,12 @@ public class Mobile implements IMobile {
 
     @Override
     public int getWidth() {
-        return 0;
+        return getDimension().getWidth();
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return getDimension().getWidth();
     }
 
     @Override
