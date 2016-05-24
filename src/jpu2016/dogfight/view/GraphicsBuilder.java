028dffc4b4bf2062a8c9a693661266c4775926fb
Shiks67 <ahmed.sdlv@gmail.com>
2016-05-24 12:08:16 +0200

    phase 2.0 en cours

--- src/jpu2016/dogfight/view/GraphicsBuilder.java
+++ src/jpu2016/dogfight/view/GraphicsBuilder.java
@@ -5,12 +5,24 @@ import jpu2016.dogfight.model.IMobile;
 import jpu2016.gameframe.IGraphicsBuilder;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 
 /**
  * Created by Ahmed on 23/05/2016.
  */
 public class GraphicsBuilder implements IGraphicsBuilder {
+    protected DogfightView dogfightView;
+    protected IDogfightModel dogfightModel;
+    //protected BufferedImage bufferedImage;
+
+    public DogfightView getDogfightView() {
+        return dogfightView;
+    }
+
+    public void setDogfightView(DogfightView dogfightView) {
+        this.dogfightView = dogfightView;
+    }
 
     public GraphicsBuilder(IDogfightModel dogfightModel){
 
--- src/jpu2016/gameframe/GameFrame.java
+++ src/jpu2016/gameframe/GameFrame.java
@@ -1,5 +1,7 @@
 package jpu2016.gameframe;
 
+import jpu2016.dogfight.view.DogfightView;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Observable;
@@ -8,6 +10,15 @@ import java.util.Observable;
  * Created by Ahmed on 23/05/2016.
  */
 public class GameFrame implements KeyListener{
+    protected DogfightView dogfightView;
+
+    public DogfightView getDogfightView() {
+        return dogfightView;
+    }
+
+    public void setDogfightView(DogfightView dogfightView) {
+        this.dogfightView = dogfightView;
+    }
 
     //GamePanel gamePanel = new GamePanel();
 
